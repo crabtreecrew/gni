660e360aa24270295a4bf0ecd0614e648bb66cdc
Dmitry Mozzherin <dmozzherin@gmail.com>
2011-11-30 17:53:14 -0500

    added in memory cache for SemanticMeaning

--- app/models/semantic_meaning.rb
+++ app/models/semantic_meaning.rb
@@ -1,2 +1,13 @@
 class SemanticMeaning < ActiveRecord::Base
+  @cache = {}
+  def self.method_missing(m, *args, &block)
+    if @cache.blank?
+      self.all.each {|sm| @cache[sm.name.to_sym] = sm.id}
+    end
+    if @cache[m]
+      @cache[m]
+    else 
+      raise NoMethodError, "No method %s for %s" % [m, self.model_name]
+    end
+  end
 end
