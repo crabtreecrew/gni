c043d057495d6aeb1babb97dd7fc1b552002bf24
David Shorthouse <davidpshorthouse@gmail.com>
2012-07-09 12:33:22 -0400

    Added EOL, Union on homepage, other UI adjustments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def title(page_title, options={})
     content_for(:title, page_title.to_s)
   end
+  def match_type(id)
+    types = {
+      1 => "exact match",
+      2 => "exact canonical match",
+      3 => "fuzzy canonical match",
+      4 => "exact canonical match (species level)",
+      5 => "fuzzy canonical match (species level)",
+      6 => "exact canonical match (genus level)"
+    }
+    types[id]
+  end
 end
