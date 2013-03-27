35972141262c02d582a8e10766ba962267052402
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-03-26 17:37:18 -1000

    typo fix

--- app/models/name_resolver.rb
+++ app/models/name_resolver.rb
@@ -831,7 +831,9 @@ private
   end
 
   def sort_data_sources(res)
-    res[:results].sort_by! { |r| [-r[:score], r[:data_source_id], r[:match_type]] }
+    res[:results].sort_by! { |r| [-r[:score], 
+      r[:data_source_id], 
+      r[:match_type]] }
     preferred_data_sources(res)
     res[:results] = [res[:results][0]] if options[:best_match_only]
   end
@@ -844,7 +846,7 @@ private
     if options[:best_match_only]
       pref_data_sources = {}
       res[:preferred_results] = res[:preferred_results].select do |r| 
-        has_source = pref_data_sources[r[:data_source_id]]
+        has_source = !pref_data_sources[r[:data_source_id]]
         pref_data_sources[r[:data_source_id]] = 1
         has_source
       end
