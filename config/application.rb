e0df8681faf9cccf09558829947b29edbf1d13e7
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-05-07 14:13:53 -0400

    travis

--- config/application.rb
+++ config/application.rb
@@ -18,44 +18,17 @@ if defined?(Bundler)
 end
 module Gni
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
     config.active_record.schema_format = :sql
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from
-    # config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my',
                                                  'locales',
                                                  '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
 
-    # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
-    # Enable the asset pipeline
     config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
     config.generators do |g|
@@ -67,7 +40,8 @@ module Gni
 
   end
 
-  Mysql2::Client.default_query_options[:connect_flags] |= Mysql2::Client::LOCAL_FILES
+  Mysql2::Client.default_query_options[:connect_flags] |=
+    Mysql2::Client::LOCAL_FILES
 
   require Rails.root.join('vendor', 'lib', 'ruby-uuid', 'uuid').to_s
   require Rails.root.join('lib', 'gni').to_s
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the
+# database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside
+# the db with db:setup).
 #
-# to create csv files use "select * from table_name into outfile 'csv_file_name'"
+# to create csv files use "select * from table_name into outfile
+# 'csv_file_name'"
 # TODO: create rake db:seed:dump
-#  
+#
+
+def locahost?
+  conf = Rails.configuration.database_configuration(Rails.env)
+  host = conf['host']
+  host && !['locahost', '127.0.0.1', '0.0.0.0'].include?(host)
+end
 
 def load_csv(dir, file)
   puts "Adding data to table %s" % file[0...-4]
   NameString.connection.execute("truncate %s" % file[0...-4])
-  local= !!(RUBY_PLATFORM =~ /darwin/) ? '' : 'local' #hack to get around a bug in mysql2 on os x
+
+  #hack to get around a bug in mysql2 on os x
+  local= !!(RUBY_PLATFORM =~ /darwin/ || localhost?) ? '' : 'local'
+
   NameString.connection.execute("
     load data %s infile '%s'
-    into table %s 
+    into table %s
     set created_at = now(), updated_at = now()
     " % [local, File.join(dir, file), file[0...-4]])
 end
