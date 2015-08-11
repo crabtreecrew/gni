df0cbeac215e088a026f507b2b621162283cd6a9
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-08-11 19:24:03 -0400

    fix #40 vernacular names in resolver result

--- app/models/vernacular_string_index.rb
+++ app/models/vernacular_string_index.rb
@@ -2,4 +2,12 @@ class VernacularStringIndex < ActiveRecord::Base
   set_primary_keys :data_source_id, :vernacular_string_id, :taxon_id
   belongs_to :vernacular_string
   belongs_to :data_source
+
+  def self.vernaculars(data_source_id, taxon_id)
+    VernacularStringIndex.connection.select(
+      "select vs.name, vsi.language, vsi.locality, vsi.country_code
+      from vernacular_string_indices vsi
+      join vernacular_strings vs on vs.id = vsi.vernacular_string_id
+      where data_source_id = %d and taxon_id = %d" % [data_source_id, taxon_id])
+  end
 end
