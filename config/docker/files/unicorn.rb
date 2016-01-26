f69d09d9af41b3a9eb17589bdf435f272bf2da04
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-01-26 11:07:56 -0600

    unicorn config fix

--- config/docker/files/unicorn.rb
+++ config/docker/files/unicorn.rb
@@ -8,9 +8,10 @@ pid "#{app_dir}/tmp/unicorn.pid"
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
-worker_processes = ENV["GNI_UNICORN_WORKER_PROCESSES"].to_i
-worker_processes = 10 if worker_processes == 0
+workers_num = ENV["GNI_UNICORN_WORKER_PROCESSES"].to_i
+workers_num = 10 if worker_processes == 0
 
-# listen 8080, :tcp_nopush => true
+worker_processes workers_num
+
 listen "/tmp/unicorn.sock", :backlog => 64
 timeout 240
