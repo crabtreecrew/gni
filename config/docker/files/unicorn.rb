2a98bf80c0e20c7f93f1dd6fc92c88be4db6cf65
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-01-26 11:42:10 -0600

    fix 3char browse

--- config/docker/files/unicorn.rb
+++ config/docker/files/unicorn.rb
@@ -9,7 +9,7 @@ stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
 workers_num = ENV["GNI_UNICORN_WORKER_PROCESSES"].to_i
-workers_num = 10 if worker_processes == 0
+workers_num = 10 if workers_num == 0
 
 worker_processes workers_num
 
